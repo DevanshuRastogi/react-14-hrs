a30684d14ab8a5053d5ac3cf3774377920786370
DevanshuRastogi <125262981+DevanshuRastogi@users.noreply.github.com>
2025-01-05 05:17:28 +0530

    ternary conditionals done

--- 01/react-js-rev-01/src/components/product/index.jsx
+++ 01/react-js-rev-01/src/components/product/index.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import SubProduct from "./components/SubProduct";
-import './style.css'
+import "./style.css";
 
-const ProductList = ({name , country , listOfProd}) => {
-   
-   console.log(`${name} ${country}`)
+const ProductList = ({ name, country, listOfProd }) => {
+  const flag = true;
+
+  // function renderTextBlock(getFlag) {
+  //   return getFlag ? (
+  //     <h1>
+  //       Hey {name} from {country}
+  //     </h1>
+  //   ) : (
+  //     <h6>hello world</h6>
+  //   );
+  // }
+
+  let renderTextBlock = flag ? (
+    <h1>
+      Hey {name} from {country}
+    </h1>
+  ) : (
+    <h6>hello world</h6>
+  );
   return (
-      <div>
-     <h1 className="head-1"> Ecommerce Projects</h1>
+    <div>
+      <h1 className="head-1"> Ecommerce Projects</h1>
+      {renderTextBlock}
+
       {/* <SubProduct/> */}
       <ul>
-        {listOfProd.map((item,index) => (
-        //   <li key ={index}>{item}</li>
-        <SubProduct singleItem = {item} key={index}/>
+        {listOfProd.map((item, index) => (
+          //   <li key ={index}>{item}</li>
+          <SubProduct singleItem={item} key={index} />
         ))}
       </ul>
     </div>
