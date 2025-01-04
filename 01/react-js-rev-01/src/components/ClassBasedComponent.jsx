80d6550aee9021af6eefa1bc088f0e2728534561
DevanshuRastogi <125262981+DevanshuRastogi@users.noreply.github.com>
2025-01-05 04:03:24 +0530

    class based and functional component basic revised

---
+++ 01/react-js-rev-01/src/components/ClassBasedComponent.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+export default class ClassBasedComponent extends Component{
+    render(){
+        return(
+            <h1>Hari Narayan</h1>
+        )
+    }
+}
+
---
+++ 01/react-js-rev-01/src/components/FunctionalComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FunctionalComponent (){
+    return(
+        <div>
+            <h2>Jai Siya Ram</h2>
+        </div>
+    )
+}
